8f6acf8a42dbcf5d997335b681aa22bd496f7906
Jonathan Bruckman <jbruckman86@gmail.com>
2022-12-05 20:54:06 -0500

    setting up routing on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
+import {Routes, Route} from 'react-router-dom'
+import UserContainer from './components/UserContainer';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import DriverDetails from './components/DriverDetails';
+import TracksContainer from './components/TracksContainer';
+import TrackDetails from './components/TrackDetails';
+import DriversContainer from './components/DriversContainer';
+
 
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Routes>
+        <Route path='/' element={<UserContainer />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/sign_up' element={<SignUp />} />
+        <Route path='/drivers' element={<DriversContainer />} />
+        <Route path='/tracks' element={<TracksContainer />} />
+      </Routes>
     </div>
+      
+    
   );
 }
 
 export default App;
+
+{/* <div className="px-2 flex justify-between items-center w-full h-full">
+  <div className="flex items-center">
+    <h1 className='text-4xl font-bold pl-8 mr-4'>Track Days</h1>
+    <ul className="flex">
+      <li>Tracks</li>
+      <li>Drivers</li>
+    </ul>
+  </div>
+  <div className='flex pr-4'>
+    <button className='border-none bg-transparent mr-4'>Sign In</button>
+    <button>Sign up</button>
+
+  </div>
+</div> */}
--- src/components/TimeTrialsContainer.jsx
+++ src/components/TimeTrialsContainer.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import DriversContainer from "./DriversContainer";
 import TracksContainer from "./TracksContainer";
 import UserContainer from ".UserContainer";
-import Signup from "./Signup";
-import Signup from './Login';
+import Signup from "./SignUp";
 
-export default function TimeTrialsContainer () {
 
-    return (
+function TimeTrialsContainer() {
+return (
     <div>
         {/* might need to add signup and login component as a ternary here instead of user */}
         {/* Signedup ? <Signup/> : <Login /> */}
@@ -17,3 +16,5 @@ export default function TimeTrialsContainer () {
     </div>
     )
 }
+
+export default TimeTrialsContainer;
