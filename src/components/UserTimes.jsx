c5273407b1efe8de9d230e4052b33523532fa2c4
Jonathan Bruckman <jbruckman86@gmail.com>
2022-12-07 16:40:02 -0500

    added to usertimes

--- src/components/UserTimes.jsx
+++ src/components/UserTimes.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Driver from "./Driver";
 
-export default function UserTimes () {
+export default function UserTimes ({user}) {
+    const { time_trials } = user
+    // const { track_id, driver_id, Lap_Time, date } = time_trials
 
     return (
         <div>
-            
+            {Driver.time_trials}
+            {/* {time_trials.date} */}
         </div>
     )
 }
