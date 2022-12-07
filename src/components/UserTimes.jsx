e25bfe89d496299a1d145b67e281138f2e0433c3
Jonathan Bruckman <jbruckman86@gmail.com>
2022-12-07 16:46:29 -0500

    refixed usertimes

--- src/components/UserTimes.jsx
+++ src/components/UserTimes.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect } from "react";
 import TimeTrial from "./TimeTrial";
 
-export default function UserTimes ({user}) {
-    const {time_trials} = user
+export default function UserTimes({ user }) {
+    const { time_trials } = user
     // const {track_id, driver_id, Lap_Time, date} = time_trials
+
+    const timeTrialCards = time_trials?.map((time_trial) => <TimeTrial key={time_trial.id} id={time_trial.id} time_trial={time_trial} />)
 
     return (
         <div>
-            
+            {timeTrialCards}
         </div>
     )
 }
