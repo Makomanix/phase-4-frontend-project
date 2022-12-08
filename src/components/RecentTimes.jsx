13b8da76e589cfa94c6917117b7a9d651f53017d
Jonathan Bruckman <jbruckman86@gmail.com>
2022-12-08 00:27:12 -0500

    more css for profile and container

--- src/components/RecentTimes.jsx
+++ src/components/RecentTimes.jsx
@@ -4,11 +4,11 @@ export default function RecentTimes ({time_trial}) {
     const { track_id, driver_id, Lap_Time, date } = time_trial
     
     return (
-        <div className="grid grid-cols-4 grid-rows-1 w-auto h-8 overflow-hidden bg-slate-400 mx-12 rounded-xl outline hover:bg-slate-200" >
-            <div className='text-center pt-1'>{date}</div>
+        <div className="grid grid-cols-4 grid-rows-1 w-auto h-8 bg-slate-400 mx-2 overflow-hidden rounded-xl outline hover:bg-slate-200" >
+            <div className='text-center pt-1'>{date}:</div>
             <div className='text-center pt-1'>{track_id}</div>
             <div className='text-center pt-1'>{driver_id}</div>
-            <div className='text-center pt-1'>{Lap_Time}</div>
+            <div className='text-center pr-4 pt-1'>{Lap_Time}</div>
     </div>
     )
 }
