50d148076f5e92354d4d043c2d0bba7e1d30da9b
Jonathan Bruckman <jbruckman86@gmail.com>
2022-12-08 00:39:43 -0500

    usertimes box added, still need data to populate

--- src/components/UserTimes.jsx
+++ src/components/UserTimes.jsx
@@ -8,8 +8,10 @@ export default function UserTimes ({user}) {
     const timeTrialCards = time_trials?.map((time_trial) => <TimeTrial key={time_trial.id} id={time_trial.id} time_trial={time_trial}/>)
 
     return (
-        <div>
+        <div className="bg-slate-200 absolute bottom-11 left-8 mr-20 h-[35%] w-[30%] rounded-md border-8 outline">
+            <div className="grid grid-cols-1 grid-rows-auto overflow-hidden py-1 gap-y-2">
             {timeTrialCards}
+            </div>
         </div>
     )
 }
